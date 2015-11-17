871a972958c62e0b0710d70729ad42bbcda4b9dd
boukassem <yassine.boukassem@devinci.fr>
2015-11-17 15:14:43 +0100

    step 4

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,14 +1,19 @@
 'use strict';
 
 var React = require('react');
-var boxStyle = {'height':100,
-'width': 100}
+var boxStyle = {'height':'100px',
+'width': '100px'}
 
 var Box = React.createClass({
   /**
    * Render a HTML button
    * @return {ReactElement}
    */
+   'getInitialState': function onGetInitialState() {
+	   return {
+		   'value': this.props.initialValue
+	   };
+   },
   'render': function onRender () {
     return (
          <button style = {boxStyle}>{this.props.value}</button>
@@ -17,3 +22,11 @@ var Box = React.createClass({
 });
 
 React.render(<Box value='X'/>, document.body);
+
+var MessageBox = React.createClass({
+  render: function() {
+    return <div>{'Mr. ' + this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(<MessageBox name="Rogers"/>, mountNode);
